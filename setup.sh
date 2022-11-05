8b1947a24d714e2a4e2705cce12c07c0733ba06b
Vinod Kumar <vinodk.info@gmail.com>
2022-11-05 22:30:43 +0530

    Removed source code

--- setup.sh
+++ setup.sh
@@ -15,11 +15,3 @@ sudo ufw status
 sudo systemctl start nginx
 sudo systemctl status nginx
 sudo systemctl enable nginx
-sudo cd /var/www/html
-sudo mkdir /tmp/sourcecode
-cd /tmp/sourcecode
-aws s3 sync s3://nphc-vinod-s3-bucket .
-sudo unzip source-code
-sudo rm -f /var/www/html/*
-sudo mv index.html index.jss style.css /var/www/html/.
-sudo rm -f source-code
